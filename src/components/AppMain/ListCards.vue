2030511ccd1f7181305bede0210e4402869957e7
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-18 23:29:19 +0200

    added getListCardsItem() and cardsList[]

--- src/components/AppMain/ListCards.vue
+++ src/components/AppMain/ListCards.vue
@@ -6,15 +6,24 @@ export default {
   data(){
     return{
         apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+        cardsList:[]
     }
   },
   components: {
     ListCardsItem
   },
   methods: {
-
+    getListCardsItem(){
+        axios.get(this.apiUrl)
+            .then( response => {
+                this.cardsList = response.data;
+                console.log('cardsList', this.cardsList)
+            })
+    }
   },
-
+  mounted(){
+    this.getListCardsItem()
+  }
 }
 </script>
 
