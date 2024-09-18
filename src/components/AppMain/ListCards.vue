3e428f284ff92347e2b3e2849fd562bfde59b3e2
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-19 00:23:03 +0200

    added template and style to ListCards

--- src/components/AppMain/ListCards.vue
+++ src/components/AppMain/ListCards.vue
@@ -28,16 +28,29 @@ export default {
 </script>
 
 <template>
-    <span>{{ cardsList.length }}</span>
-    <ul>
-        <ListCardsItem v-for="card in cardsList" :key="card.id"
-        :name = 'card.name'
-        :type = 'card.archetype'
-        :imgUrl = 'card.card_images[0].image_url'
-        />
-    </ul>
+    <div class="container">
+        <p class="card-found">Found <span><b>{{ cardsList.length }}</b></span> cards</p>
+        <ul id="card-list">
+            <ListCardsItem v-for="card in cardsList" :key="card.id"
+            :name = 'card.name.toUpperCase()'
+            :type = 'card.archetype'
+            :imgUrl = 'card.card_images[0].image_url'
+            />
+        </ul>
+    </div>
 </template>
 
 <style lang="scss">
+    .card-found{
+        background-color: #000;
+        color: #fff;
+        padding:15px 10px;
+        font-size: 12px;
+    }
+    #card-list{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 15px;
+    }
 
 </style>
