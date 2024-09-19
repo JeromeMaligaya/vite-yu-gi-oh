16c47b238b16b29afd9dcc7d605dcf2f60435871
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-19 23:09:23 +0200

    moved getListCardsItem() and apiUrl from ListCards to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,14 @@
 <script>
 import ListCards from './AppMain/ListCards.vue';
 import MainSelect from './AppMain/MainSelect.vue';
+import axios from 'axios';
+
 
 export default {
   data(){
     return{
-
+      apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+      cardsList:[],
     }
   },
   components: {
@@ -13,7 +16,17 @@ export default {
     MainSelect
   },
   methods: {
-
+    getListCardsItem(){
+      axios.get(this.apiUrl)
+        .then( response => {
+          this.cardsList = response.data.data;
+          console.log('cardsList', this.cardsList)
+        }
+      )
+    }
+  },
+  mounted(){
+    this.getListCardsItem()
   },
 }
 </script>
@@ -21,7 +34,7 @@ export default {
 <template>
     <main>
         <MainSelect />
-        <ListCards />
+        <ListCards :cardsList="cardsList"/>
     </main>
 </template>
 
--- src/components/AppMain/ListCards.vue
+++ src/components/AppMain/ListCards.vue
@@ -1,29 +1,25 @@
 <script>
 import ListCardsItem from './ListCardsItem.vue';
-import axios from 'axios';
 
 export default {
   data(){
     return{
-        apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
-        cardsList:[],
+  
+    }
+  },
+  props: {
+    cardsList:{
+      type: Array,
+      Required:true
     }
   },
   components: {
     ListCardsItem
   },
   methods: {
-    getListCardsItem(){
-        axios.get(this.apiUrl)
-            .then( response => {
-                this.cardsList = response.data.data;
-                console.log('cardsList', this.cardsList)
-            })
-    }
+   
   },
-  mounted(){
-    this.getListCardsItem()
-  }
+ 
 }
 </script>
 
