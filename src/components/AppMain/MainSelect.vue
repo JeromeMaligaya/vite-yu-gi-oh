26cdb2611c68efbebc4408e0a062c2ab0eb3a5b1
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-19 22:18:55 +0200

    added template and style on MainSelect

--- src/components/AppMain/MainSelect.vue
+++ src/components/AppMain/MainSelect.vue
@@ -15,9 +15,27 @@ export default {
 </script>
 
 <template>
-
+    <div class="container">
+        <select name="" id="select-archetype">
+            <option selected disabled>Choose an archetype</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use "./../../style/general.scss" as *;
+
+.container{
+    margin-bottom: 30px;
+    background-color: #D78D44;
+
+    #select-archetype{
+        width: 150px;
+        height: 25px;
+    }
+}
 
 </style>
